c35d7348f20e663aa6f0b10e7c57a3dcc9edd341
David O Neill <daoneill@redhat.com>
2022-07-21 19:22:09 +0100

    readme

--- do-expense.sh
+++ do-expense.sh
@@ -2,7 +2,7 @@
 MONTH=$(date +%B)
 python3 virgin.py
 pdftotext $MONTH.pdf
-bill_date=$(sed -nr "s#We'll collect your payment on (.*)#\1#p" July.txt)
+bill_date=$(sed -nr "s#We'll collect your payment on (.*)#\1#p" $MONTH.txt)
 bill_date=$(date -d"$bill_date" +%m/%d/%Y)
 python3 concur.py --bill_date $bill_date --receipt $MONTH.pdf
 
